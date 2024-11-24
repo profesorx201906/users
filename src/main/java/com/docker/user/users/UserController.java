c918c76f8a6865e468a85cba545b5efc85356957
Javier Díaz Díaz <jddiazsena@gmail.com>
2024-11-24 12:15:37 -0500

    primer commit

---
+++ src/main/java/com/docker/user/users/UserController.java
@@ -0,0 +1,41 @@
+package com.docker.user.users;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @GetMapping
+  public List<User> getAllUsers() {
+    return userRepository.findAll();
+  } 
+
+  @GetMapping("/saludar")
+  public String saludar() {
+    return "hola Mundo";
+  } 
+
+  @GetMapping("/{id}")
+  public User getUserById(@PathVariable Long id) {
+    return userRepository.findById(id).get();
+  }
+
+  @PostMapping
+  public User createUser(@RequestBody User user) {
+    return userRepository.save(user);
+  }
+}
